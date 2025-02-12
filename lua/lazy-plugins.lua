ed134cbed21f12109a6e60b9c57ee51d62e2c75b
Risang Baskoro <contact@risangbaskoro.com>
2025-02-13 04:33:41 +0700

    Enable treesitter

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -13,7 +13,7 @@ require('lazy').setup({
     require 'base/plugins/neo-tree',
     require 'base/plugins/todo-comments',
     require 'base/plugins/mini',
-    -- require 'base/plugins/treesitter',
+    require 'base/plugins/treesitter',
   
     -- require 'base.plugins.debug',
     -- require 'base.plugins.indent_line',
