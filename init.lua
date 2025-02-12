5e3404aa069094760979c78f55c96e9905e77633
Risang Baskoro <contact@risangbaskoro.com>
2025-02-13 02:55:32 +0700

    Install plugin manager

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ require 'options'
 require 'keymaps'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
--- require 'lazy-bootstrap'
+require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 -- require 'lazy-plugins'
